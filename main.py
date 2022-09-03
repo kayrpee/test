0d9509e249c973c4892f8a0dd9139a81cd6de3a4
Kevyn <kayrpee@users.noreply.github.com>
2022-09-03 19:51:25 -0400

    Display number and play music

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def on_button_pressed_a():
+    music.play_melody("E B C5 A B G A F ", 120)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+basic.show_number(6)
+basic.pause(10000)
+basic.clear_screen()
+
 def on_forever():
     pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    music.playMelody("E B C5 A B G A F ", 120)
+})
+basic.showNumber(6)
+basic.pause(10000)
+basic.clearScreen()
 basic.forever(function on_forever() {
     
 })
